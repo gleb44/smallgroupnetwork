2afe53a58893adbc487f668fb2e837086798c1f3
gyazykov <gyazykov@gmail.com>
2016-11-11 10:54:42 +0300

    Update build

--- src/client/app/app.routes.ts
+++ src/client/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 
 import { AboutRoutes } from './about/index';
 import { HomeRoutes } from './home/index';
-import { LoginRoutes } from "./login/index";
-import { AdminRoutes } from "./admin/index";
+import { LoginRoutes } from './login/index';
+import { AdminRoutes } from './admin/index';
 
 export const routes: Routes = [
   ...HomeRoutes,
